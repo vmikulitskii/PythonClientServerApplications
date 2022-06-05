c4e12244819fa784d11cb965a39fac536fbf6fc8
Vladimir <mikulex@mail.ru>
2022-06-05 12:57:18 +0500

    Убрал хардкод

--- log/client_log_config.py
+++ log/client_log_config.py
@@ -1,8 +1,9 @@
-import logging
+import logging,os
 
 LOG = logging.getLogger('client')
 
-HANDLER = logging.FileHandler('log/client.log', encoding='utf-8')
+log_file = os.path.join(os.path.dirname(__file__),'client.log')
+HANDLER = logging.FileHandler(log_file, encoding='utf-8')
 FORMATTER = logging.Formatter('%(asctime)s - %(levelname)8s - %(name)s - %(message)s')
 HANDLER.setFormatter(FORMATTER)
 LOG.addHandler(HANDLER)
--- log/server_log_config.py
+++ log/server_log_config.py
@@ -1,9 +1,11 @@
-import logging
+import logging,os
 import logging.handlers
 
 LOG = logging.getLogger('server')
 
-HANDLER = logging.handlers.TimedRotatingFileHandler('log/server.log', encoding='utf-8', interval=1, when='D')
+
+log_file = os.path.join(os.path.dirname(__file__),'server.log')
+HANDLER = logging.handlers.TimedRotatingFileHandler(log_file, encoding='utf-8', interval=1, when='D')
 FORMATTER = logging.Formatter('%(asctime)s - %(levelname)8s - %(name)s - %(message)s')
 HANDLER.setFormatter(FORMATTER)
 LOG.addHandler(HANDLER)
